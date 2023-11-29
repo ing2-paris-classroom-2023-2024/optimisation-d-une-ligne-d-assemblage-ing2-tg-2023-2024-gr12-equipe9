15c28b78ff594ed19fcb5b42319841762471e046
Sowgan <131462087+BENOITWilliam@users.noreply.github.com>
2023-11-29 20:54:39 +0100

    Update graph.c
    début du graph.c

--- graph.c
+++ graph.c
@@ -1,56 +1,107 @@
-#ifndef GRAPHE_H_INCLUDED
-#define GRAPHE_H_INCLUDED
+#include "Graphe.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-
-
-/* Structure d'un arc*/
-struct Arc
+/* affichage des successeurs du sommet num*/
+void afficher_successeurs(pSommet * sommet, int num)
 {
-    int sommet; // numéro de sommet d'un arc adjacent au sommet initial
-    int valeur;
-    struct Arc* arc_suivant;
-};
 
-/* Alias de pointeur sur un Arc */
-typedef struct Arc* pArc;
+    printf(" sommet %d :\n",num);
 
-/* Structure d'un sommet*/
-struct Sommet
+    pArc arc=sommet[num]->arc;
+
+    while(arc!=NULL)
+    {
+        printf("%d ",arc->sommet);
+        arc=arc->arc_suivant;
+    }
+
+}
+
+// Ajouter l'arête entre les sommets s1 et s2 du graphe
+pSommet* CreerArete(pSommet* sommet,int s1,int s2)
 {
-    struct Arc* arc;
-    int valeur;
-    char couleur;
+    if(sommet[s1]->arc==NULL)
+    {
+        pArc Newarc=(pArc)malloc(sizeof(struct Arc));
+        Newarc->sommet=s2;
+        Newarc->arc_suivant=NULL;
+        sommet[s1]->arc=Newarc;
+        return sommet;
+    }
 
-};
+    else
+    {
+        pArc temp=sommet[s1]->arc;
+        while( !(temp->arc_suivant==NULL))
+        {
+            temp=temp->arc_suivant;
+        }
+        pArc Newarc=(pArc)malloc(sizeof(struct Arc));
+        Newarc->sommet=s2;
+        Newarc->arc_suivant=NULL;
 
-/* Alias de pointeur sur un Sommet */
-typedef struct Sommet* pSommet;
+        if(temp->sommet>s2)
+        {
+            Newarc->arc_suivant=temp->arc_suivant;
+            Newarc->sommet=temp->sommet;
+            temp->sommet=s2;
+            temp->arc_suivant=Newarc;
+            return sommet;
+        }
 
-/* Alias d'un Graphe */
-typedef struct Graphe
-{
-    int taille;
-    int orientation;
-    int ordre;
-    pSommet* pSommet;
-} Graphe;
+        temp->arc_suivant=Newarc;
+        return sommet;
+    }
+}
 
 // créer le graphe
-Graphe* CreerGraphe(int ordre);
+Graphe* CreerGraphe(int ordre)
+{
+    Graphe * Newgraphe=(Graphe*)malloc(sizeof(Graphe));
+    Newgraphe->pSommet = (pSommet*)malloc(ordre*sizeof(pSommet));
+
+    for(int i=0; i<ordre; i++)
+    {
+        Newgraphe->pSommet[i]=(pSommet)malloc(sizeof(struct Sommet));
+        Newgraphe->pSommet[i]->valeur=i;
+        Newgraphe->pSommet[i]->arc=NULL;
+    }
+    return Newgraphe;
+}
+
 
 /* La construction du réseau peut se faire à partir d'un fichier dont le nom est passé en paramètre
 Le fichier contient : ordre, taille,orientation (0 ou 1)et liste des arcs */
-Graphe * lire_graphe(char * nomFichier);
+Graphe * lire_graphe(char * nomFichier)
+{
+    Graphe* graphe;
+    FILE * ifs = fopen(nomFichier,"r");
+    int taille, orientation, ordre, s1, s2;
 
-// Ajouter l'arête entre les sommets s1 et s2 du graphe
-pSommet* CreerArete(pSommet* sommet,int s1,int s2);
+    if (!ifs)
+    {
+        printf("Erreur de lecture fichier\n");
+        exit(-1);
+    }
 
-/* affichage des successeurs du sommet num*/
-void afficher_successeurs(pSommet * sommet, int num);
+    fscanf(ifs,"%d",&ordre);
 
-/*affichage du graphe avec les successeurs de chaque sommet */
-void graphe_afficher(Graphe* graphe);
+    graphe=CreerGraphe(ordre); // créer le graphe d'ordre sommets
 
-#endif // GRAPHE_H_INCLUDED
+    fscanf(ifs,"%d",&taille);
+    fscanf(ifs,"%d",&orientation);
+
+    graphe->orientation=orientation;
+    graphe->ordre=ordre;
+
+    // créer les arêtes du graphe
+    for (int i=0; i<taille; ++i)
+    {
+        fscanf(ifs,"%d%d",&s1,&s2);
+        graphe->pSommet=CreerArete(graphe->pSommet, s1, s2);
+
+        if(!orientation)
+            graphe->pSommet=CreerArete(graphe->pSommet, s2, s1);
+    }
+
+    return graphe;
+}
