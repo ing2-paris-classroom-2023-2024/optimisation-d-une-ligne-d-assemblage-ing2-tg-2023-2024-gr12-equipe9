86a3b590586d579375b1565bb4c7042008da6166
paulgandelin <131462237+paulgandelin@users.noreply.github.com>
2023-11-30 00:23:04 +0100

    Update and rename contrainte to contrainte.c et intégration avec le .h

--- contrainte.c
+++ contrainte.c
@@ -1,4 +1,4 @@
-/* fonction permettant de vérifie la contrainte d'exclusion entre deux sommets*/
+*/* fonction permettant de vérifie la contrainte d'exclusion entre deux sommets*/
 int exclus_present(int ope,t_station* station,t_graphe *g){
     pArc temparc;
     int temp_ope,i;
