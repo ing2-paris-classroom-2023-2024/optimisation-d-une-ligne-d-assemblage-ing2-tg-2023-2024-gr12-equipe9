fc515c41d7d508a33ac214b7e402d410b6658b42
Narcy91 <145811507+Narcy91@users.noreply.github.com>
2023-11-30 21:29:25 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,8 @@
+Graphe * g;
+    station* station_;
+
+    char nom_fichier_temps,nom_fichier_exclusion,nom_fichier_precedences,nom_fichier_cycle;
+    nom_fichier_temps = "texte/operations.txt";
+    nom_fichier_exclusion = "texte/exclusions.txt";
+    nom_fichier_precedences = "texte/precedences.txt";
+    nom_fichier_cycle = "texte/temps_cycle.txt";
