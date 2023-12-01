ac6cd04fc883cc9cd330de026201a6485ac033b3
paulgandelin <131462237+paulgandelin@users.noreply.github.com>
2023-12-01 19:20:12 +0100

    rangement fichier

--- affichage.c
+++ affichage.c
@@ -1,1 +1,17 @@
+#include "Graphe.h"
 
+/* affichage des successeurs du sommet num*/
+void afficher_successeurs(pSommet * sommet, int num)
+{
+
+    printf(" sommet %d :\n",num);
+
+    pArc arc=sommet[num]->arc;
+
+    while(arc!=NULL)
+    {
+        printf("%d ",arc->sommet);
+        arc=arc->arc_suivant;
+    }
+
+}
