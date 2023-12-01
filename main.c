b748fe9aec5f5a09fbb280226d5a3b9737315724
Sowgan <131462087+BENOITWilliam@users.noreply.github.com>
2023-12-01 09:10:56 +0100

    Update main.c
    ouverture du fichier + station initiale

--- main.c
+++ main.c
@@ -6,3 +6,11 @@ Graphe * g;
     nom_fichier_exclusion = "texte/exclusions.txt";
     nom_fichier_precedences = "texte/precedences.txt";
     nom_fichier_cycle = "texte/temps_cycle.txt";
+
+g = lire_graphe(nom_fichier_temps,nom_fichier_exclusion,nom_fichier_precedences,nom_fichier_cycle);
+
+    /// afficher le graphe
+    grapheafficher(g);
+
+    station = creerligne(g);
+    station = station_->station_initial;
