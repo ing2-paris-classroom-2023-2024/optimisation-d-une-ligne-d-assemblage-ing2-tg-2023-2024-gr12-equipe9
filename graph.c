19e18f5af0f544c7161c7195f522748d5a716004
paulgandelin <131462237+paulgandelin@users.noreply.github.com>
2023-12-01 19:24:05 +0100

    amélioration newgraph

--- graph.c
+++ graph.c
@@ -38,18 +38,29 @@ pSommet* CreerArete(pSommet* sommet,int s1,int s2)
     }
 }
 /* créer le graphe */
-Graphe* CreerGraphe(int ordre)
+t_graphe* CreerGraphe(FILE *fichier)
 {
-    int i;
-    Graphe * Newgraphe=(Graphe*)malloc(sizeof(Graphe));
-    Newgraphe->pSommet = (pSommet*)malloc(ordre*sizeof(pSommet));
-
-    for(i=0; i<ordre; i++)
-    {
-        Newgraphe->pSommet[i]=(pSommet)malloc(sizeof(struct Sommet));
-        Newgraphe->pSommet[i]->valeur=i;
-        Newgraphe->pSommet[i]->arc=NULL;
+    t_graphe * Newgraphe;
+    int i,som1;
+    float temps;
+    Newgraphe=(t_graphe*)malloc(sizeof(t_graphe));
+    Newgraphe->pSommet=(pSommet*)malloc(50*sizeof(t_sommet));
+    Newgraphe->ordre=0;
+    i=0;
+    som1=0;
+    while (feof(fichier)==0){
+        fscanf(fichier,"%d %f", &som1, &temps);
+        Newgraphe->ordre++;
+        Newgraphe->pSommet[i] = (pSommet) malloc(sizeof(t_sommet));
+        Newgraphe->pSommet[i]->ope = som1;
+        Newgraphe->pSommet[i]->duree = temps;
+        Newgraphe->pSommet[i]->station = -1;
+        Newgraphe->pSommet[i]->nb_pred = 0;
+        Newgraphe->pSommet[i]->arc = NULL;
+        Newgraphe->pSommet[i]->predecesseur = (int*) malloc(5*sizeof(int));
+        i++;
     }
+    Newgraphe->ordre--;
     return Newgraphe;
 }
 
