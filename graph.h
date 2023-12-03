a1c5484298ef5f5b68b472a6d76dbc9fab41c4ec
Narcy91 <145811507+Narcy91@users.noreply.github.com>
2023-12-03 17:45:39 +0100

    Update graph.h

--- graph.h
+++ graph.h
@@ -1,6 +1,3 @@
-//
-// Created by kylia on 16/11/2023.
-//
 
 #ifndef INIT_GRAPH_H
 #define INIT_GRAPH_H
@@ -57,6 +54,7 @@ typedef struct station
 
 
 
+//initialisation du graphe avec lecture de fichier
 // créer le graphe
 t_graphe* CreerGraphe(FILE *fichier);
 
@@ -67,38 +65,39 @@ t_graphe * lire_graphe(char *nomFichier, char *nomFichier2, char *nomFichier3,ch
 // Ajouter l'arête entre les sommets s1 et s2 du graphe
 pSommet* CreerArete(pSommet* sommet,int s1,int s2);
 
+int ope_existe(int sommet, t_graphe *graphe);
+
+
+
+
+//gestion des stations
+t_station *initialisation(void);
+t_station* creer_ligne(t_graphe *graphe);
+void ajout_sommet(t_station *Station, pSommet sommet,t_graphe *graphe);
+void nouvelle_station(t_station *Station);
+
+
+//gestion des contraintes
+int est_fini(t_graphe *g);
+int exclus_present(int ope,t_station* station,t_graphe *g);
+int pred_present(int ope,t_station* station,t_graphe *g);
+
+
+
+
+///affichage
 /* affichage des successeurs du sommet num*/
 void afficher_successeurs(pSommet * sommet, int num,t_graphe* graphe );
 
 /*affichage du graphe avec les successeurs de chaque sommet */
 void graphe_afficher(t_graphe* graphe);
 
-int sommet_en_ope(int ope, t_graphe *graphe);
-
-int ope_en_sommet(int ope, t_graphe *graphe);
-
-t_station* num_station(int num, t_station* station_);
-
-t_station* creer_ligne(t_graphe *graphe);
-
-int est_fini(t_graphe *g);
-
+/* affichage de la station selectionne */
 void afficher_station(t_station *Station);
-
-void ajout_sommet(t_station *Station, pSommet sommet,t_graphe *graphe);
-
-void nouvelle_station(t_station *Station);
-
-t_station *initialisation(void);
-
-int exclus_present(int ope,t_station* station,t_graphe *g);
-
-int pred_present(int ope,t_station* station,t_graphe *g);
 
 void affichage_erreur_op (t_graphe* graphe);
 void erreur_lecture (char *nomFichier);
 void erreur_fichier(int operation, const char* nom_fichier);
-int ope_existe(int sommet, t_graphe *graphe);
 void affichage_contraite(int choix);
 void affichage_menu_contrainte(void);
 void affichage_menu_station_sommet(void);
@@ -107,4 +106,15 @@ void erreur_choix_operation(void);
 void choix_station(void);
 void erreur_choix_station(void);
 void affichage_staion(void);
+
+
+// conversion entre sommet et operation
+int sommet_en_ope(int ope, t_graphe *graphe);
+int ope_en_sommet(int ope, t_graphe *graphe);
+
+//recherche de la station
+t_station* num_station(int num, t_station* station_);
+
+
+
 #endif //INIT_GRAPH_H
